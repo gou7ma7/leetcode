67a2f491a05d978d48bb418fdae882df9334c5d2
lihongrui <6104109142@qq.com>
2020-09-03 10:57:13 +0800

    feat(success the base 01 solution): use two point taolu or use hash to replace the find 3rd cycle.

--- [15]3Sum/01.third_cycle2hash.py
+++ [15]3Sum/01.third_cycle2hash.py
@@ -11,16 +11,17 @@ class Solution:
         # print(nums_dict)
         result_list = []
         for left in range(nums_len - 1):
-            # if nums[left] == nums[left + 1]:
-            #     continue
+            if left and nums[left] == nums[left - 1]:
+                continue
             for right in range(left + 1, nums_len):
-                # if right < nums_len - 1 and nums[right] == nums[right + 1]:
-                #     continue
+                # print('left_index', left, nums[left], 'right_index', right, nums[right])
+                if right > left + 1 and nums[right] == nums[right - 1]:
+                    continue
 
                 tow_sum = -(nums[left] + nums[right])
-                print('left_index', left, nums[left], 'right_index', right, nums[right], tow_sum)
                 tow_sum_index = nums_dict.get(tow_sum)
                 if tow_sum_index and right < tow_sum_index:
+                    # print('left_index', left, nums[left], 'right_index', right, nums[right], 'tow_sum_index', tow_sum_index, tow_sum)
                     result_list.append([nums[left], nums[right], tow_sum])
 
         return result_list
@@ -28,5 +29,10 @@ class Solution:
 
 # leetcode submit region end(Prohibit modification and deletion)
 if __name__ == '__main__':
+    print(Solution().threeSum([0, 0, 0, 0]))
     print(Solution().threeSum([0, 0, 0]))
     print(Solution().threeSum([-1, 0, 1, 2, -1, -4]))
+
+    # 解答成功:
+    # 执行耗时: 1476 ms,击败了32.69% 的Python3用户
+    # 内存消耗: 18MB, 击败了15.08 % 的Python3用户
